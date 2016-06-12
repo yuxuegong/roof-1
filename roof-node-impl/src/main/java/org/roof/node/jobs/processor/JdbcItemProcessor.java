52ae43d3a2e8910607a038ddf198512782a62431
lhttanner <lht_ge@163.com>
2016-06-12 18:10:06 +0800

    串联

--- roof-node-impl/src/main/java/org/roof/node/jobs/processor/JdbcItemProcessor.java
+++ roof-node-impl/src/main/java/org/roof/node/jobs/processor/JdbcItemProcessor.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.roof.commons.RoofDateUtils;
@@ -13,27 +14,28 @@ import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemProcessor;
 
-public class JdbcItemProcessor implements ItemProcessor<Page, Page> {
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+
+public class JdbcItemProcessor implements ItemProcessor<Page, Map> {
 	private StepExecution stepExecution;
+	private Object templateVo;
 
 	@Override
-	public Page process(Page page) throws Exception {
+	public Map<String, Object> process(Page page) throws Exception {
 		Collection<?> collection = page.getDataList();
 		JobParameters jobParameters = stepExecution.getJobParameters();// 获得job启动参数
-		String collection_code = jobParameters.getString("collection_code");// 采集编码
-		String index_code = jobParameters.getString("index_code");// 指标编码
-		String dev_code = jobParameters.getString("dev_code");// 设备编码
-
-		for (Object object : collection) {
-			if (object instanceof Map) {
-				Map values = (Map) object;
-				values.put("collection_code", collection_code);
-				values.put("index_code", index_code);
-				values.put("dev_code", dev_code);
-				values.put("collection_time", new Date());
-			}
-		}
-		return page;
+		String html = "<p>Hello, my name is Alan.Today is . I am from Somewhere, TX. I have  kids:</p><ul><li>1:Jimmy is 12</li><li>2:Sally is 4</li></ul>"
+				+ "<h1>reuse templete</h1>" + "<p>Home page</p>" + "<span>Powered by Handlebars.java</span>";
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("file", html);
+		map.put("hosts", "10.80.19.204");
+		map.put("user", "root");
+		map.put("password", "zjhcsoft");
+		map.put("port", "22");
+		map.put("remoteDirectory", "soft");
+		map.put("remoteFileSeparator", "/");
+		return map;
 	}
 
 	@BeforeStep
--- roof-node-impl/src/main/java/org/roof/node/jobs/writer/ChannelPageItemWriter.java
+++ roof-node-impl/src/main/java/org/roof/node/jobs/writer/ChannelPageItemWriter.java
@@ -1,16 +1,24 @@
 package org.roof.node.jobs.writer;
 
 import java.util.List;
+import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
-import org.roof.roof.dataaccess.api.Page;
+import javax.annotation.Resource;
+
+import org.roof.file.upload.integration.api.UploadFile;
+import org.roof.file.upload.integration.api.UploadFileOperate;
+import org.roof.file.upload.integration.api.UploadFileServiceI;
+import org.roof.file.upload.integration.api.UploadTarget;
+import org.roof.file.upload.integration.api.UploadType;
+import org.roof.file.upload.integration.impl.UploadFileImpl;
+import org.roof.file.upload.integration.impl.UploadTargetImpl;
 import org.roof.spring.CurrentSpringContext;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.messaging.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.GenericMessage;
 
 /**
  * 将分页写入通道 <br>
@@ -20,39 +28,51 @@ import org.springframework.messaging.support.GenericMessage;
  * @author liuxin
  *
  */
-public class ChannelPageItemWriter implements ItemWriter<Page> {
+public class ChannelPageItemWriter implements ItemWriter<Map> {
 
-	private MessageChannel outChannel;
 	private StepExecution stepExecution;
 
+	private UploadFileServiceI uploadFileService;
+
 	@Override
-	public void write(List<? extends Page> items) throws Exception {
-		String customOutChannelName = stepExecution.getJobParameters().getString("outChannel");
-		MessageChannel customOutChannel = null;
-		if (StringUtils.isNotEmpty(customOutChannelName)) {
-			customOutChannel = CurrentSpringContext.getBean(customOutChannelName, MessageChannel.class);
+	public void write(List<? extends Map> items) throws Exception {
+		String html = "<p>Hello, my name is Alan.Today is . I am from Somewhere, TX. I have  kids:</p><ul><li>1:Jimmy is 12</li><li>2:Sally is 4</li></ul>"
+				+ "<h1>reuse templete</h1>" + "<p>Home page</p>" + "<span>Powered by Handlebars.java</span>";
+
+		UploadTarget target = new UploadTargetImpl(UploadType.Sftp, "10.80.19.204", "root", "zjhcroot", 22, "soft",
+				"/");
+
+		UploadFile<?> file = new UploadFileImpl(target, "", html, "xxx.html", UploadFileOperate.REPLACE);
+		if(uploadFileService == null){
+			uploadFileService = CurrentSpringContext.getBean("uploadFileService", UploadFileServiceI.class);
 		}
-		for (Page page : items) {
-			Message<Page> message = new GenericMessage<Page>(page);
-			if (customOutChannel != null) {
-				customOutChannel.send(message);
-			} else {
-				outChannel.send(message);
-			}
-		}
+		uploadFileService.Upload(file);
+		// String customOutChannelName =
+		// stepExecution.getJobParameters().getString("outChannel");
+		// MessageChannel customOutChannel = null;
+		// if (StringUtils.isNotEmpty(customOutChannelName)) {
+		// customOutChannel = CurrentSpringContext.getBean(customOutChannelName,
+		// MessageChannel.class);
+		// }
+
+		// for (Page page : items) {
+		// Message<Page> message = new GenericMessage<Page>(page);
+		// if (customOutChannel != null) {
+		// customOutChannel.send(message);
+		// } else {
+		// outChannel.send(message);
+		// }
+		// }
+	}
+
+	@Resource(name = "uploadFileService")
+	public void setUploadFileService(UploadFileServiceI uploadFileService) {
+		this.uploadFileService = uploadFileService;
 	}
 
 	@BeforeStep
 	public void setStepExecution(StepExecution stepExecution) {
 		this.stepExecution = stepExecution;
-	}
-
-	public MessageChannel getOutChannel() {
-		return outChannel;
-	}
-
-	public void setOutChannel(MessageChannel outChannel) {
-		this.outChannel = outChannel;
 	}
 
 }

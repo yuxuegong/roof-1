cc1e3c2e02423daa17c744a3fee7c1d8cb33be8b
zz3310969 <675523646@qq.com>
2018-01-09 17:57:44 +0800

    update

--- roof-web-base/roof-web-base-webapp/src/main/java/org/roof/web/menu/action/MenuAction.java
+++ roof-web-base/roof-web-base-webapp/src/main/java/org/roof/web/menu/action/MenuAction.java
@@ -11,14 +11,20 @@ import org.roof.web.menu.entity.MenuDto;
 import org.roof.web.menu.entity.MenuType;
 import org.roof.web.menu.entity.MenuVo;
 import org.roof.web.menu.service.api.IMenuService;
+import org.roof.web.menu.service.impl.MenuFilter;
 import org.roof.web.resource.entity.ResourceVo;
 import org.roof.web.resource.service.api.IResourceService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("base/menu")
@@ -27,10 +33,8 @@ public class MenuAction {
 	@Autowired
 	private IResourceService resourceService;
 
-	@RequestMapping("/index")
-	public String index() {
-		return "/roof-web/web/menu/menu_index.jsp";
-	}
+	@Autowired
+	private MenuFilter menuFilter;
 
 	@RequestMapping(value = "base",method = {RequestMethod.GET})
 	public @ResponseBody Result base() {
@@ -107,6 +111,15 @@ public class MenuAction {
 		menuService.update(parent);
 		menuService.update(menu);
 		return new Result("移动成功!");
+	}
+
+	@RequestMapping( method = {RequestMethod.GET})
+	public @ResponseBody
+	Result menu(HttpServletRequest request) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Menu menu = menuFilter.doFilter(1L, new UsernamePasswordAuthenticationToken(authentication.getPrincipal(),
+				authentication.getCredentials(), authentication.getAuthorities()));
+		return new Result(Result.SUCCESS, menu);
 	}
 
 	@Autowired(required = true)
--- roof-web-base/roof-web-base-webapp/src/main/java/org/roof/web/resource/action/ResourceAction.java
+++ roof-web-base/roof-web-base-webapp/src/main/java/org/roof/web/resource/action/ResourceAction.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.roof.spring.Result;
 import org.roof.web.dictionary.entity.Dictionary;
 import org.roof.web.dictionary.entity.DictionaryVo;
+import org.roof.web.resource.entity.Module;
 import org.roof.web.resource.entity.Privilege;
 import org.roof.web.resource.entity.Resource;
 import org.roof.web.resource.entity.ResourceVo;
@@ -23,105 +24,116 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("base/resource")
 public class ResourceAction {
-	private static final Logger logger = LoggerFactory.getLogger(ResourceAction.class);
-	private IResourceService resourceService;
-	private IResourcesUtils resourcesUtils;
+    private static final Logger logger = LoggerFactory.getLogger(ResourceAction.class);
+    private IResourceService resourceService;
+    private IResourcesUtils resourcesUtils;
 
 
-	@RequestMapping(value = "/{id}", method = {RequestMethod.GET})
-	public @ResponseBody Result<Privilege> load(@PathVariable Long id) {
-		Privilege resource = resourceService.load(id);
-		if (resource.getParent() != null && resource.getParent().getId() != null) {
-			resource.setParent(resourceService.load(resource.getParent().getId()));
-		}
-		return new Result(Result.SUCCESS,resource);
-	}
+    @RequestMapping(value = "/{id}", method = {RequestMethod.GET})
+    public @ResponseBody
+    Result<Privilege> load(@PathVariable Long id) {
+        Privilege resource = resourceService.load(id);
+        if (resource.getParent() != null && resource.getParent().getId() != null) {
+            resource.setParent(resourceService.load(resource.getParent().getId()));
+        }
+        return new Result(Result.SUCCESS, resource);
+    }
 
 
-	/**
-	 * 创建一个新的资源
-	 *
-	 * @return
-	 */
-	@RequestMapping(method = {RequestMethod.POST})
-	public @ResponseBody Result create(@RequestBody Privilege privilege) {
-		try {
-			Long parentId = privilege.getParent().getId();
-			String type = privilege.getDtype();
-			resourceService.create(parentId, type);
-			return new Result(Result.SUCCESS, "新增成功!");
-		}catch (Exception e){
-			logger.error("新增资源",e);
-			return new Result(Result.FAIL, "新增失败");
-		}
+    /**
+     * 创建一个新的资源
+     *
+     * @return
+     */
+    @RequestMapping(method = {RequestMethod.POST})
+    public @ResponseBody
+    Result create(@RequestBody Privilege privilege) {
+        try {
+            Long parentId = privilege.getParent().getId();
+            String type = privilege.getDtype();
+            resourceService.create(parentId, type);
+            return new Result(Result.SUCCESS, "新增成功!");
+        } catch (Exception e) {
+            logger.error("新增资源", e);
+            return new Result(Result.FAIL, "新增失败");
+        }
 
-	}
+    }
 
-	@RequestMapping(value="/{id}",method = {RequestMethod.DELETE})
-	public @ResponseBody Result delete(@PathVariable Long id) {
-		resourceService.delete(id);
-		return new Result("删除成功!");
-	}
+    @RequestMapping(value = "/{id}", method = {RequestMethod.DELETE})
+    public @ResponseBody
+    Result delete(@PathVariable Long id) {
+        resourceService.delete(id);
+        return new Result("删除成功!");
+    }
 
-	@RequestMapping("/updateModule")
-	public @ResponseBody Result updateModule(Integer initBasic, Privilege module) {
-		if (module != null && module.getId() != null) {
-			resourceService.updateIgnoreNull(module);
-			module = (Privilege) resourceService.load(module.getId());
-			if (module.getLeaf() != null && initBasic != null && module.getLeaf() && initBasic == 1) {
-				resourcesUtils.initBasicOperation(module);
-				module.setLeaf(false);
-				resourceService.updateIgnoreNull(module);
-			}
-		}
-		return new Result("保存成功!");
-	}
+    @RequestMapping(value = "/init/module/{id}", method = {RequestMethod.POST})
+    public @ResponseBody
+    Result initModule(@PathVariable Long id) {
+        Privilege module = (Privilege) resourceService.load(id);
+        resourcesUtils.initBasicOperation(module);
+        module.setLeaf(false);
+        resourceService.updateIgnoreNull(module);
+        return new Result("保存成功!");
+    }
 
-	@RequestMapping("/updatePrivilege")
-	public @ResponseBody Result updatePrivilege(Privilege privilege) {
-		if (privilege != null) {
-			resourceService.updateIgnoreNull(privilege);
-		}
-		return new Result("保存成功!");
-	}
+    @RequestMapping(value = "/module/{id}", method = {RequestMethod.PUT})
+    public @ResponseBody
+    Result updateModule(@PathVariable Long id, @RequestBody Privilege module) {
+        module.setId(id);
+        module.setDtype(Module.class.getSimpleName());
+        resourceService.updateIgnoreNull(module);
+        return new Result("保存成功!");
 
-	@RequestMapping(value = "/tree",method = {RequestMethod.GET})
-	public @ResponseBody Result read(Long parentId) {
-		List<Privilege> resources = resourceService.findPrivilegeByParent(parentId);
-		return new Result(Result.SUCCESS,resources);
-	}
+    }
 
-	/**
-	 * 根据父节点ID加载资源列表页面
-	 * 
-	 * @return
-	 */
-	@RequestMapping("/read")
-	public @ResponseBody List<ResourceVo> read(Long id, Long roleId, HttpServletRequest request, Model model) {
-		String basePath = request.getContextPath();
-		if (roleId == null || roleId.longValue() == 0) {
-			return resourceService.read(id, basePath);
-		} else {
-			return resourceService.readByRole(id, roleId, basePath);
-		}
-	}
+    @RequestMapping(value = "/privilege/{id}", method = {RequestMethod.PUT})
+    public @ResponseBody
+    Result updatePrivilege(@PathVariable Long id, @RequestBody Privilege privilege) {
+        privilege.setId(id);
+        resourceService.updateIgnoreNull(privilege);
+        return new Result("保存成功!");
+    }
 
-	public IResourceService getResourceService() {
-		return resourceService;
-	}
+    @RequestMapping(value = "/tree", method = {RequestMethod.GET})
+    public @ResponseBody
+    Result read(Long parentId) {
+        List<Privilege> resources = resourceService.findPrivilegeByParent(parentId);
+        return new Result(Result.SUCCESS, resources);
+    }
 
-	public IResourcesUtils getResourcesUtils() {
-		return resourcesUtils;
-	}
+    /**
+     * 根据父节点ID加载资源列表页面
+     *
+     * @return
+     */
+    @RequestMapping("/read")
+    public @ResponseBody
+    List<ResourceVo> read(Long id, Long roleId, HttpServletRequest request, Model model) {
+        String basePath = request.getContextPath();
+        if (roleId == null || roleId.longValue() == 0) {
+            return resourceService.read(id, basePath);
+        } else {
+            return resourceService.readByRole(id, roleId, basePath);
+        }
+    }
 
-	@Autowired(required = true)
-	public void setResourceService(@Qualifier("resourceService") IResourceService resourceService) {
-		this.resourceService = resourceService;
-	}
+    public IResourceService getResourceService() {
+        return resourceService;
+    }
 
-	@Autowired(required = true)
-	public void setResourcesUtils(@Qualifier("resourcesUtils") IResourcesUtils resourcesUtils) {
-		this.resourcesUtils = resourcesUtils;
-	}
+    public IResourcesUtils getResourcesUtils() {
+        return resourcesUtils;
+    }
+
+    @Autowired(required = true)
+    public void setResourceService(@Qualifier("resourceService") IResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
+
+    @Autowired(required = true)
+    public void setResourcesUtils(@Qualifier("resourcesUtils") IResourcesUtils resourcesUtils) {
+        this.resourcesUtils = resourcesUtils;
+    }
 
 }
